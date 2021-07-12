0719f47f17bc6e130d88d2743567c0bf7fb85dca
jsta <stachel2@msu.edu>
2021-07-12 08:10:34 -0600

    linting

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-albers_conic <- function(){
+albers_conic <- function() {
   # Albers Equal Area Conic
   "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 }
@@ -13,14 +13,14 @@ albers_conic <- function(){
 #'
 #' # custom setting
 #' Sys.setenv(
-#' LAGOSUSGIS_PATH = path.expand("~/.local/share/LAGOS-GIS/gis_locus_v1.0.gpkg"))
+#'   LAGOSUSGIS_PATH = path.expand("~/.local/share/LAGOS-GIS/gis_locus_v1.0.gpkg"))
 #' lagosusgis_path()
 #'
 #' # revert to defaults
 #' Sys.setenv(LAGOSUSGIS_PATH = "")
 #' lagosusgis_path()
 #' }
-lagosusgis_path <- function(){
+lagosusgis_path <- function() {
   res_default <- file.path(
     rappdirs::user_data_dir("LAGOS-GIS"), "LAGOS_US_GIS_Data_v0.7.gdb")
 
@@ -28,20 +28,20 @@ lagosusgis_path <- function(){
 
   if (any(!env_vars == "")) {
     res <- env_vars
-  }else{
+  } else {
     res <- res_default
   }
 
   res
 }
 
-lagosusgis_dir <- function(){
+lagosusgis_dir <- function() {
   rappdirs::user_data_dir("LAGOS-GIS")
 }
 
 #' @importFrom curl curl_fetch_memory
 #' @importFrom stringr str_extract
-get_file_names <- function(url){
+get_file_names <- function(url) {
   handle <- curl::new_handle(nobody = TRUE)
 
   headers <- curl::parse_headers(
@@ -52,44 +52,44 @@ get_file_names <- function(url){
   gsub('\\"', "", res)
 }
 
-get_if_not_exists <- function(url, destfile, overwrite){
-  if(!file.exists(destfile) | overwrite){
+get_if_not_exists <- function(url, destfile, overwrite) {
+  if (!file.exists(destfile) | overwrite) {
     download.file(url, destfile, mode = "wb")
-  }else{
+  } else {
     message(paste0("A local copy of ", url, " already exists on disk"))
   }
 }
 
 #' @importFrom sf st_transform st_crs st_is_longlat st_coordinates st_centroid
-toUTM <- function(sf_object){
+toUTM <- function(sf_object) {
 
-  if(is.na(st_crs(sf_object)$epsg)){
+  if (is.na(st_crs(sf_object)$epsg)) {
     sf_object <- st_transform(sf_object, crs = 4326)
   }
 
-  if(sf::st_is_longlat(sf_object)){
+  if (sf::st_is_longlat(sf_object)) {
     suppressWarnings(
       utm_zone <- long2UTM(st_coordinates(st_centroid(st_union(sf_object)))[1]))
     crs      <- paste0("+proj=utm +zone=", utm_zone, " +datum=WGS84")
 
     sf::st_transform(sf_object, crs = crs)
-  }else{
+  } else {
     sf_object
   }
 }
 
 long2UTM <- function(long) {
-  (floor((long + 180)/6) %% 60) + 1
+  (floor((long + 180) / 6) %% 60) + 1
 }
 
-has_7z <- function(){
+has_7z <- function() {
   paths_7z <- c("7z",
-                "~/usr/bin/7z",
-                "C:\\PROGRA~1\\7-Zip\\7za",
-                "C:\\PROGRA~1\\7-Zip\\7z.exe")
-  if(!any(nchar(Sys.which(paths_7z)) > 0)){
+    "~/usr/bin/7z",
+    "C:\\PROGRA~1\\7-Zip\\7za",
+    "C:\\PROGRA~1\\7-Zip\\7z.exe")
+  if (!any(nchar(Sys.which(paths_7z)) > 0)) {
     list(yes = FALSE, path = NA)
-  }else{
+  } else {
     list(yes = TRUE, path = paths_7z[nchar(Sys.which(paths_7z)) > 0])
   }
 }
@@ -114,10 +114,10 @@ has_7z <- function(){
 #' @export
 #' @return Either the local path of the downloaded file (default), or a raw
 #'   vector
-gh_file <- function(url = NULL, ref=NULL,
+gh_file <- function(url = NULL, ref = NULL,
                     owner = NULL, repo = NULL, path = NULL,
-                    to_disk=TRUE, destfile=NULL,
-                    .token = NULL, .api_url= NULL, .method="GET",
+                    to_disk = TRUE, destfile = NULL,
+                    .token = NULL, .api_url = NULL, .method = "GET",
                     .limit = NULL, .send_headers = NULL) {
   if (!is.null(url)) {
     matches <- stringi::stri_match_all_regex(
@@ -131,7 +131,7 @@ gh_file <- function(url = NULL, ref=NULL,
     pathfile <- basename(path)
   }
   pathdir <- dirname(path)
-  if(length(grep("/", path)) == 0){
+  if (length(grep("/", path)) == 0) {
     pathdir <- NULL
   }
 
