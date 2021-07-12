1d2ae007d1a25126e95323adae58e3566ca0ebe4
jsta <stachel2@msu.edu>
2021-07-12 08:53:32 -0600

    linting

--- R/query.R
+++ R/query.R
@@ -35,37 +35,37 @@
 #'
 #' # query lake polygon from coordinates using extent argument
 #' lake_coordinates <- query_gis("LAGOS_US_All_Lakes_1ha_points",
-#'                               "lagoslakeid", "1")
+#'   "lagoslakeid", "1")
 #'
 #' lake_polygon <- query_gis("LAGOS_US_All_Lakes_1ha",
-#'                           extent = sf::st_bbox(lake_coordinates))
+#'   extent = sf::st_bbox(lake_coordinates))
 #' }
 query_gis <- function(layer, id_name = NULL, ids = NULL, extent = character(0),
-                                       crs = albers_conic(),
-                                       gis_path = lagosusgis_path()){
+                      crs = albers_conic(),
+                      gis_path = lagosusgis_path()) {
 
-  if(all(is.null(id_name), is.null(ids))){
-    if(length(extent) > 0){
+  if (all(is.null(id_name), is.null(ids))) {
+    if (length(extent) > 0) {
       wkt_filter <- sf::st_as_sfc(extent, crs = albers_conic())
       res <- sf::st_read(gis_path, layer = layer,
-                         wkt_filter = sf::st_as_text(wkt_filter), quiet = TRUE)
-    }else{
+        wkt_filter = sf::st_as_text(wkt_filter), quiet = TRUE)
+    } else {
       res <- sf::st_read(gis_path, layer = layer, quiet = TRUE)
     }
-  }else{
+  } else {
     res <- query_gis_(gis_path,
-               query = paste0("SELECT * FROM ", layer,
-                              " WHERE ", id_name, " IN ('",
-                              paste0(ids,
-                                     collapse = "', '"), "')"),
-               extent = extent,
-               crs = crs)
+      query = paste0("SELECT * FROM ", layer,
+        " WHERE ", id_name, " IN ('",
+        paste0(ids,
+          collapse = "', '"), "')"),
+      extent = extent,
+      crs = crs)
 
     # sort items by ids
-    res <- res[match(ids, data.frame(res[,id_name])[,id_name]),]
+    res <- res[match(ids, data.frame(res[, id_name])[, id_name]), ]
   }
 
-  if(any(unique(sf::st_geometry_type(sf::st_geometry(res))) == "MULTISURFACE")){
+  if (any(unique(sf::st_geometry_type(sf::st_geometry(res))) == "MULTISURFACE")) {
     res <- sf::st_cast(res, "MULTIPOLYGON")
   }
 
@@ -95,17 +95,17 @@ query_gis <- function(layer, id_name = NULL, ids = NULL, extent = character(0),
 #' # query multiple nested hucs
 #' hu4s  <- query_gis("hu4", "hu4_huc4", c("0415", "0414"))
 #' hu8s  <- query_gis_(query = paste0("SELECT * FROM hu8 WHERE ",
-#'             paste0("hu8_huc8 LIKE '", hu4s$hu4_huc4, "%'", collapse = " OR ")))
+#'   paste0("hu8_huc8 LIKE '", hu4s$hu4_huc4, "%'", collapse = " OR ")))
 #'
 #' # query lake polygon from coordinates using extent argument
 #' lake_coordinates <- query_gis("LAGOS_US_All_Lakes_1ha_points",
-#'                               "lagoslakeid", "1")
+#'   "lagoslakeid", "1")
 #' lake_polygon <- query_gis_(query = "SELECT * FROM LAGOS_US_All_Lakes_1ha",
-#'                            extent = sf::st_bbox(lake_coordinates))
+#'   extent = sf::st_bbox(lake_coordinates))
 #' }
 #'
 query_gis_ <- function(gis_path = lagosusgis_path(), query, extent = character(0),
-                       crs = albers_conic()){
+                       crs = albers_conic()) {
 
   # error if extent is not NA and query is defined?
 
@@ -115,16 +115,16 @@ query_gis_ <- function(gis_path = lagosusgis_path(), query, extent = character(0
   # gis_path <- path.expand("~/.local/share/LAGOS-GIS/LAGOS_US_GIS_Data_v0.7.gdb")
   # sf::st_layers(gis_path)
 
-  if(!file.exists(gis_path) & !dir.exists(gis_path)){
-  stop(paste0("Data not available at the path pointed to by lagosusgis_path():\n  '",
-                lagosusgis_path(), "'"))
+  if (!file.exists(gis_path) & !dir.exists(gis_path)) {
+    stop(paste0("Data not available at the path pointed to by lagosusgis_path():\n  '",
+      lagosusgis_path(), "'"))
   }
 
   ###
   layer <- stringr::str_extract(query, "(?<=FROM )(.*)(?= WHERE)")
   dat <- sf::st_read(gis_path, layer = layer, query = query,
-                     wkt_filter = sf::st_as_text(sf::st_as_sfc(extent)),
-                     quiet = TRUE)
+    wkt_filter = sf::st_as_text(sf::st_as_sfc(extent)),
+    quiet = TRUE)
 
   # sf::st_crs(dat)      <- sf::st_crs(crs)
   #
@@ -153,31 +153,30 @@ query_gis_ <- function(gis_path = lagosusgis_path(), query, extent = character(0
 #' mapview(res)
 #'
 #' res <- query_wbd(lagoslakeid = c(2057, 3866, 1500, 3386, 2226,
-#' 1637, 6874, 7032, 1935, 6970, 5331, 34352))
-#' res <- res[res$lagoslakeid == 34352,]
+#'   1637, 6874, 7032, 1935, 6970, 5331, 34352))
+#' res <- res[res$lagoslakeid == 34352, ]
 #' mapview::mapview(res)
-#'
 #' }
 query_wbd <- function(lagoslakeid, gis_path = lagosusgis_path(),
-                      crs = albers_conic(), utm = FALSE){
+                      crs = albers_conic(), utm = FALSE) {
 
   iws           <- query_gis("ws", "lagoslakeid", lagoslakeid)
   lake_boundary <- query_gis("LAGOS_US_All_Lakes_1ha", "lagoslakeid", lagoslakeid)
 
   res <- lapply(seq_len(nrow(iws)), function(x) {
-          iws_dissolve  <- st_buffer(do.call(c, st_geometry(iws)[x]), 0)
-          lake_dissolve <- st_buffer(do.call(c, st_geometry(lake_boundary)[x]), 0.001)
+    iws_dissolve  <- st_buffer(do.call(c, st_geometry(iws)[x]), 0)
+    lake_dissolve <- st_buffer(do.call(c, st_geometry(lake_boundary)[x]), 0.001)
 
-          st_geometry(st_union(iws_dissolve, lake_dissolve))
-          })
+    st_geometry(st_union(iws_dissolve, lake_dissolve))
+  })
 
   res <- do.call(c, res)
   st_crs(res) <- crs
   st_geometry(iws) <- res
 
-  if(utm){
+  if (utm) {
     toUTM(iws)
-  }else{
+  } else {
     iws
   }
 }
@@ -195,13 +194,13 @@ query_wbd <- function(lagoslakeid, gis_path = lagosusgis_path(),
 #' res <- query_lake_poly(c(474805, 474803))
 #' }
 query_lake_poly <- function(lagoslakeid, gis_path = lagosusgis_path(),
-                      crs = albers_conic(), utm = FALSE){
+                            crs = albers_conic(), utm = FALSE) {
 
   res <- query_gis("LAGOS_US_All_Lakes_1ha", "lagoslakeid", lagoslakeid)
 
-  if(utm){
+  if (utm) {
     toUTM(res)
-  }else{
+  } else {
     res
   }
 }
